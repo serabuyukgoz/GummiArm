669fc7f422c81596f52a7b33383bf20236fc9bf3
sera <sera.buyukgoz@postgrad.plymouth.ac.uk>
2017-07-29 14:03:44 +0100

    finding peaks with velocity

--- orchestration/packages/src/gummi_interface/scripts/a_gummi_signal.py
+++ orchestration/packages/src/gummi_interface/scripts/a_gummi_signal.py
@@ -4,94 +4,170 @@ import rospy
 import sys
 import random
 import time
+
+import numpy as np 
+
 from math import pi
 
 from gummi_interface.gummi import Gummi
 from dynamixel_msgs.msg import JointState
 
-s = 0
-t = False
+class Reflex:
+	def __init__(self, name):
+		self.name = name
+		self.data = 0.0
+		self.oldData = 0.0
+		self.index = 0
+		self.lists = [0.0]
+		self.flag = False
 
-def Callback(msg):
-    global t
-    global s
-    hit_signal = msg.load
-        #print "Load: " + str(hit_signal)
+		self.amplitude = 0.0
+		self.hitFlag = False
 
-    if t == True:
-        if  hit_signal >= 0.5:
-            print ("Hit from left <--")
-            t = False
-            s = 1
+		self.gummi = Gummi()
+		self.r = rospy.Rate(60)
 
-        if hit_signal <= -0.5:
-            print ("Hit from right --> ")
-            t = False
-            s = 2
-         
+		rospy.Subscriber(self.name + "_controller/state", JointState, self.Callback)
+
+	def Callback(self, msg):
+
+		if self.flag == True:
+			signal = msg.velocity
+			print(signal)
+			self.filterData(signal)		
+
+
+	def filterData(self, data):
+
+		if data > 0.1:
+			data = data
+		else:
+			if self.index > 0:
+				data = data
+			else:
+				data = 0.0
+
+		self.findWave(data)
+	
+
+	def findWave(self, data):
+
+		self.data = data
+
+		if self.data > 0.0:
+			self.lists.append(self.data)
+			self.index += 1
+			if self.index > 3:
+				self.oldData = self.lists[self.index-2]
+			else:
+				self.oldData = self.data
+
+		elif self.data == 0.0:
+			if self.oldData == 0.0:
+				if len(self.lists) > 1:
+					self.findAmplitude(self.lists)
+					self.index = 0
+					self.lists = [0.0]
+			else:
+				self.lists.append(self.data)
+				self.index += 1
+				if self.index > 3:
+					self.oldData = self.lists[self.index-2]
+				else:
+					self.oldData = self.data
+
+	def findAmplitude(self, array):
+		print(array)
+		maxEle = max(array)
+		minELe = min(array)
+
+		self.amplitude = abs(maxEle - minELe) / 2
+		#self.hitFlag = True
+
+		print("Amplitude:  " + str(self.amplitude))
+		#self.move()
+
+	def move(self):
+		self.flag = False
+		print("Reflex Activated")
+		for i in range(0,100):
+			self.gummi.elbow.moveWith(0.002, 0.5)
+			self.r.sleep()	
+		print("End of Reflex Motion")
+		self.flag = True	
+
+	def setFlag(self, data):
+		self.flag = data
+
+	def getFlag(self):
+		return self.hitFlag
+
+	# def setJoint(self):
+
+	# 	return jointName
 
 def main(args):
-    global t
-    global s
 
     pi = 3.1416
 
     rospy.init_node('GummiArm', anonymous=True)
     r = rospy.Rate(60)  
 
-    rospy.Subscriber("/shoulder_pitch_encoder_controller/state", JointState, Callback)
-
     gummi = Gummi()
+    reflex = Reflex("/elbow_encoder")
+
+    #After first motiom
 
     print('WARNING: Moving joints sequentially to equilibrium positions.')
-    gummi.doGradualStartup()
+    #gummi.doGradualStartup()
     
     print('WARNING: Moving to resting pose, hold arm!')
     rospy.sleep(1)
 
     gummi.goRestingPose(True)
+
     for i in range(0,400):
         gummi.goRestingPose(False)
         r.sleep()
 
-    gummi.setCollisionResponses(False, False, False, False, False)
+
     print("GummiArm is live!")
 
     for i in range(0,100):
         gummi.shoulderRoll.servoTo(pi/3, 0.6)
-        r.sleep()
+        gummi.elbow.servoTo(-pi/4, 0.6)
+        r.sleep()        
 
- 	
-    print("Try servoTo for shoulderpithch")
+	gummi.setCollisionResponses(False, False, False, False, False)
+    print("GummiArm is live!")
+    rospy.sleep(1)
 
-    t = True
-
-    for i in range(0,100):
-        gummi.elbow.servoTo(-pi/2, 0.6)
-        r.sleep()
+    reflex.setFlag(True)
 
     while not rospy.is_shutdown():
 
-       # if gummi.teleop == 0:
-       #     gummi.doUpdate()
 
-        if s == 1:
-            for i in range(0,100):
-                gummi.shoulderYaw.servoTo(-pi/3, 0.6)
-                r.sleep()
+		if gummi.teleop == 0:
+			gummi.doUpdate()
 
-            rospy.sleep(1)
-            t = True
+		# flag = reflex.getFlag()
 
-        if s == 2:
-            for i in range(0,100):
-                gummi.shoulderYaw.servoTo(pi/3, 0.6)
-                r.sleep()
-            rospy.sleep(1)
-            t = True
+		# if flag == True:
+		# 	reflex.setFlag(False)
+		#  	print("Start")
 
-        gummi.publishJointState()
-        r.sleep()
+		#  	for i in range(0,100):
+		# 		#gummi.shoulderRoll.servoTo(pi/3, 0.6)
+		# 		gummi.elbow.moveWith(0.002, 0.5)
+		# 		r.sleep()
+
+		# 	reflex.setFlag(True) 
+		# 	print("End of Reflex") 
+
+
+
+		gummi.publishJointState()
+		r.sleep()
   
 if __name__ == '__main__':
 	main(sys.argv)
